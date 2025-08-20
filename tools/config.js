17d9db92d872f5af0860692c1648e9c7c905d81d
Delta689 <di360ray@gmail.com>
2025-08-20 10:48:53 -0600

    Update config.js

--- tools/config.js
+++ tools/config.js
@@ -1,8 +1,9 @@
 window.__LUX_CONFIG__ = {
-  // Poner api de google cuando termine
-  GOOGLE_MAPS_API_KEY: "",
 
-  // Bases para la media:
+  window.__LUX_CONFIG__ = {
+  GOOGLE_MAPS_API_KEY: "AIzaSyA3FdtSv10vlNbLOq-8_-ERN4mtwe5apNk",
+
+  // (deja tus bases si las usas en otras páginas)
   MEDIA_BASE:  "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/",
   IMAGES_BASE: "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/images/",
   VIDEO_BASE:  "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/media/"
