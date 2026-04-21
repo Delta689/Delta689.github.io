1a89d22373ec4fcd7b182d06e0e6c43f2c0aea4a
Delta689 <di360ray@gmail.com>
2026-04-21 10:25:58 -0600

    Delete tools directory

--- tools/config.js
+++
@@ -1,9 +0,0 @@
-window.__LUX_CONFIG__ = {
-
-  GOOGLE_MAPS_API_KEY: "AIzaSyA3FdtSv10vlNbLOq-8_-ERN4mtwe5apNk",
-
-  // bases
-  MEDIA_BASE:  "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/",
-  IMAGES_BASE: "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/images/",
-  VIDEO_BASE:  "https://pub-bd7613c8c4cf4633acde18714ce2b3f3.r2.dev/media/"
-};
